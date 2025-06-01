29fec8872dfd3d9b3ef0aad29db5c4e32abaa245
komal-0527 <komalkalsoom27@gmail.com>
2025-06-01 17:25:17 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -11,3 +11,20 @@ function openTab(tabName) {
 }
 
 
+
+// mobile menu logic
+ const menuToggle = document.getElementById("menu-toggle");
+  const navLinks = document.getElementById("nav-links");
+
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+    const icon = menuToggle.querySelector("i");
+
+    if (navLinks.classList.contains("active")) {
+      icon.classList.remove("fa-bars");
+      icon.classList.add("fa-times");
+    } else {
+      icon.classList.remove("fa-times");
+      icon.classList.add("fa-bars");
+    }
+  });
